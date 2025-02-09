439f4af1a86d9eeb51e10cad6a6c55df92b32799
AkbarxonFayzullayev <akbarxonfayzullayev555@gmail.com>
2025-02-09 18:40:54 +0500

    repositoriya  tayyorr

---
+++ app2_1/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+def func2_11(request):
+    return HttpResponse("Salom Django,bu 2-Appdagi 1-funksiyammm")
+def func2_22(request):
+    return HttpResponse("Salom,Bu 2-Appdagi ikkinchi funksiyammm")
+def func2_33(request):
+    return HttpResponse("Salom,Bu 2- Appdagi 3-funksiyammm")
--- app2_3/views.py
+++ app2_3/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-def func3_1(request):
+def func3_11(request):
     return HttpResponse("Salom Django,bu 3-Appdagi 1-funksiyam")
-def func3_2(request):
+def func3_22(request):
     return HttpResponse("Salom,Bu 3-Appdagi ikkinchi funksiyam")
-def func3_3(request):
+def func3_33(request):
     return HttpResponse("Salom,Bu 3- Appdagi 3-funksiyam")
